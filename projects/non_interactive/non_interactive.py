caf3001dd2e7ff5dc78df0dada7fa1f411c02d77
Ethan Nelson <36721383+einelson@users.noreply.github.com>
2021-10-09 08:38:18 -0400

    Update non_interactive.py
    added descriptions

--- projects/non_interactive/non_interactive.py
+++ projects/non_interactive/non_interactive.py
@@ -19,13 +19,7 @@ layout = html.Div([
                     html.H1('Non interactive projects', className='title'),
                     html.P('All non interactive projects are linked to github, just click on the image. These projects were ones that I was not able to host on the web, or were not easy to demonstrateable on the web. I hope you enjoy these as well!', className='paragraph'),
                 ],
-            ),
-            # picture
-            dbc.Col(
-                [
-                    html.Img(src=app.get_asset_url('/data/collage_generator/Nelson3.png'), className='image'),
-                ],
-            ),            
+            ),           
         ],
     ),
 
@@ -36,7 +30,7 @@ layout = html.Div([
             dbc.Col(
                 [
                     html.H2('Point cloud classification', className='title'),
-                    html.P('description needed', className='paragraph'),
+                    html.P('The point cloud project was research into if a neural network could learn off of spatial, point cloud data. Data uesd was lidar data from warehouses and was edged towards detection of shelves, pallets, products and walls.', className='paragraph'),
                 ],
             ),
             # picture link
@@ -55,13 +49,13 @@ layout = html.Div([
             dbc.Col(
                 [
                     html.H2('EEG eye-state classification', className='title'),
-                    html.P('description needed', className='paragraph'),
+                    html.P('This was probably one of my more favorite projects. I love EEG and in this project I wanted to test how well a 2D neural network could learn EEG patterns in the brain. Take a look at the research paper I wrote on this by clicking on the image to the right. ', className='paragraph'),
                 ],
             ),
             # picture link
             dbc.Col(
                 [
-                    html.A([html.Img(src=app.get_asset_url('/data/non_interactive/eeg_eye_state_map.png'), className='image')], href='https://github.com/einelson/EEG-Eye-State-data-set'),
+                    html.A([html.Img(src=app.get_asset_url('/data/non_interactive/eeg_eye_state_map.png'), className='image')], href='https://github.com/einelson/EEG-Eye-State-data-set/blob/master/eeg.ipynb'),
                 ],
             ),
         ],style={'margin-top': '4em'},
@@ -74,7 +68,7 @@ layout = html.Div([
             dbc.Col(
                 [
                     html.H2('RSA Encryption', className='title'),
-                    html.P('description needed', className='paragraph'),
+                    html.P('A simple encryption algorithm using RSA encryption', className='paragraph'),
                 ],
             ),
             # picture link
